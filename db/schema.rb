b64138990ba5cf9bc7ba88f857f14e028db224ad
Alik <alikrugl11@gmail.com>
2021-02-10 09:48:44 +0200

    Version updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_05_211539) do
+ActiveRecord::Schema.define(version: 2021_02_08_152922) do
 
   create_table "expenses", force: :cascade do |t|
     t.string "name", null: false
